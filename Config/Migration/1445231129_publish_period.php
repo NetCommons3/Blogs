5afe9a7169dca571ae81949ab8fad2d99e18ad2b
RyujiAMANO <ryuji@ryus.co.jp>
2015-10-21 17:42:42 +0900

    add comment

--- Config/Migration/1445231129_publish_period.php
+++ Config/Migration/1445231129_publish_period.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * PublishPeriod
+ *
+ * @author   Ryuji AMANO <ryuji@ryus.co.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ */
+
+/**
+ * Class PublishPeriod
+ */
 class PublishPeriod extends CakeMigration {
 
 /**
