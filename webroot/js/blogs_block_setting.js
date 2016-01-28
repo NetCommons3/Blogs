5c8119950c37055af4a659871dddc63be4714307
RyujiAMANO <ryuji@ryus.co.jp>
2016-01-28 10:39:32 +0900

    fix JavaScriptのminify対策

--- webroot/js/blogs_block_setting.js
+++ webroot/js/blogs_block_setting.js
@@ -1,18 +1,20 @@
-NetCommonsApp.controller('Blogs.BlockSetting', function($scope) {
+NetCommonsApp.controller('Blogs.BlockSetting',
+    ['$scope', function($scope) {
 
-  /**
-   * Use like button
-   *
-   * @return {void}
-   */
-  $scope.useLike = function() {
-    var likeElement = $('#BlogSettingUseLike');
-    var unlikeElement = $('#BlogSettingUseUnlike');
-    if (likeElement[0].checked) {
-      unlikeElement[0].disabled = false;
-    } else {
-      unlikeElement[0].disabled = true;
-      unlikeElement[0].checked = false;
-    }
-  };
-});
+      /**
+       * Use like button
+       *
+       * @return {void}
+       */
+      $scope.useLike = function() {
+        var likeElement = $('#BlogSettingUseLike');
+        var unlikeElement = $('#BlogSettingUseUnlike');
+        if (likeElement[0].checked) {
+          unlikeElement[0].disabled = false;
+        } else {
+          unlikeElement[0].disabled = true;
+          unlikeElement[0].checked = false;
+        }
+      };
+    }]
+);
